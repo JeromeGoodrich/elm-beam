6c31c5ef1038e7693c4ee27159dfe497d6c725c1
Kofi Gumbs <h.kofigumbs@gmail.com>
2017-03-26 10:21:16 -0500

    Optimize Binop functions

--- src/codegen.erl
+++ src/codegen.erl
@@ -12,10 +12,24 @@ make_forms(ElmMod, Defs) ->
 
 
 from_def(ModuleName, {'Def', {'PVar', Name}, Body}) ->
-    { _, F } = from_expr(new_env(), Body),
-    { cerl:c_fname(qualifed_name(ModuleName, Name), 0),
-      cerl:c_fun([], F)
-    }.
+    case is_operator(Name) of
+        true  -> { Args, CBody } = collectLambdas(new_env(), Body, []),
+                 { cerl:c_fname(qualifed_name(ModuleName, Name), length(Args)),
+                   cerl:c_fun(Args, CBody)
+                 };
+        false -> { cerl:c_fname(qualifed_name(ModuleName, Name), 0),
+                   cerl:c_fun([], from_expr(new_env(), Body))
+                 }
+    end.
+
+
+
+collectLambdas(Env, {'Lambda', Pattern, Body}, Acc) ->
+    { NewEnv, Arg } = from_pattern(Env, Pattern),
+    collectLambdas(NewEnv, Body, Acc ++ [Arg]);
+
+collectLambdas(Env, Body, Acc) ->
+    { Acc, from_expr(Env, Body) }.
 
 
 
@@ -23,33 +37,32 @@ from_def(ModuleName, {'Def', {'PVar', Name}, Body}) ->
 
 
 from_expr(Env, {'ELit', Literal}) ->
-    { Env, from_literal(Literal) };
+    from_literal(Literal);
 
 from_expr(Env, {'EVar', Var}) ->
-    { Env, from_var(Var) };
+    from_var(Var);
 
 from_expr(Env, {'Binop', Var, Left, Right}) ->
-    { Env1, L } = from_expr(Env, Left),
-    { Env2, R } = from_expr(Env1, Right),
-    { Env2, cerl:c_apply(cerl:c_apply(from_var(Var), [L]), [R]) };
+    Op = case Var of
+            {'Variable', {'TopLevel', Home}, Name} ->
+                cerl:c_var({ qualifed_name(Home, Name), 2 });
+            {'Variable', {'Module', Home}, Name} ->
+                cerl:c_var({ qualifed_name(Home, Name), 2 })
+        end,
+    cerl:c_apply(Op, [from_expr(Env, Left), from_expr(Env, Right)]);
 
 from_expr(Env, {'Lambda', Pattern, Body}) ->
-    { Env1, P } = from_pattern(Env, Pattern),
-    { Env2, B } = from_expr(Env1, Body),
-    { Env2, cerl:c_fun([P], B) };
+    { NewEnv, P } = from_pattern(Env, Pattern),
+    cerl:c_fun([P], from_expr(NewEnv, Body));
 
 from_expr(Env, {'App', Function, Arg}) ->
-    { Env1, F } = from_expr(Env, Function),
-    { Env2, A } = from_expr(Env1, Arg),
-    { Env2, cerl:c_apply(F, [A]) };
+    cerl:c_apply(from_expr(Env, Function), [from_expr(Env, Arg)]);
 
 from_expr(Env, {'List', Elems}) ->
     F = fun(E, Acc) ->
-                { _, Expr } = from_expr(Env, E),
-                cerl:c_cons(Expr, Acc)
+                cerl:c_cons(from_expr(Env, E), Acc)
         end,
-
-    { Env, lists:foldr(F, cerl:c_nil(), Elems) }.
+    lists:foldr(F, cerl:c_nil(), Elems).
 
 
 
@@ -103,6 +116,10 @@ new_env() ->
 
 fresh_var(Env) ->
     { Env + 1, cerl:c_var(Env) }.
+
+
+is_operator(<<First/utf8, _/binary>>) ->
+    lists:member(First, "+-/*=.<>:&|^?%~!").
 
 
 qualifed_name(ModuleName, Name) ->
